166bc2161738a95a4a1db305bdfbc6b91270be6e
Athul AV <athulav111@gmail.com>
2026-02-10 17:40:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+
+// 8-2-2026
+// console.log(`Hello`);
+// console.log(`I like `);
+// window.alert(`This is an alert`)
+// window.alert(`This is`)
+
+//This is a comment
+
+/* THIS
+IS 
+COMMENT
+*/
+
+
+// document.getElementById("myH1").textContent=`Hello`;
+// document.getElementById("myP").textContent=`Ilike pizza`;
+
+
+//2-Variable
+// variable = A container that stores a value.
+//                   Behaves as if it were the value it contains.
+
+
+// let age =25;
+// let price=10.99;
+// let gpa=2.1;
+// let firstName ="Bro"
+
+// console.log(typeof firstName);
+// console.log(`Yor name is ${firstName}`)
+
+// console.log(`You are ${age} years old`)
+
+// console.log(`The ${price} `)
+
+// console.log(`You are ${age} years old`)
+
+//---------------------------------9-2-2026------------------
+
+// arithmetic operators = operands (values, variables, etc.)
+//                                         operators (+ - * /)
+//                                         ex. 11 = x + 5;
+
+
+let students =31
+
+
+//students = students + 1;
+
+// students = students - 1;
+ //students = students + 2 ;
+// students = students /2 ;
+//students = students **2;
+
+//students = students %3;
+//students = students **2;
+//let extraStudents =students % 3;
+
+//students +=1;
+//students-=1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//students++;
+//students--;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+
+
+//console.log(students);
+
+
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+// ------------------------- EASY WAY -------------------------
+
+// let username = window.prompt("What's your username?");
+
+// console.log(username);
+
+// --------------------- PROFESSIONAL WAY ---------------------
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`
+}
